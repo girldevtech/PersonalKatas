a4904a58bae52f735b12337d52e6a75de5624efb
Sarah Powell <slhpowell@gmail.com>
2012-02-15 09:03:40 -0500

    attempting to add first RangeKata to repo

---
+++ Range/Range/Range.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Range
+{
+    public class Range
+    {
+        private char[] _validDelimiters = { '[', ']', '(', ')' };
+
+        public string InputRange
+        {
+            get;
+            set;
+        }
+
+        private List<int> _points;
+        public List<int> Points
+        {
+            get
+            {
+                if (_points == null)
+                {
+                    _points = new List<int>();
+                }
+                return _points;
+            }
+            set
+            {
+                _points = value;
+            }
+        }
+
+        public bool ValidInput(string input)
+        {
+            char[] delimiters = {input[0],
+                                    input[input.Length-1]};
+
+            if (_validDelimiters.Contains(delimiters[0]) &&
+                _validDelimiters.Contains(delimiters[1]))
+            {
+                string[] integers = input.TrimEnd(_validDelimiters).TrimStart(_validDelimiters).Split(',');
+                bool canParse = true;
+                foreach (string i in integers)
+                {
+                    int parsed;
+                    canParse = int.TryParse(i, out parsed);
+                }
+                return canParse;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool ContainsList(List<int> integerList)
+        {
+            char[] inclusiveDelimiters = { '[', ']' };
+            string[] inputRange = InputRange.Split(',');
+
+            //need to determine all numbers within the range, 
+            //then can determine if the list supplied is part of that list
+        }
+
+        public List<int> GetAllPoints()
+        {
+            return Points;
+        }
+    }
+}
---
+++ Range/Range/RangeTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Range
+{
+    [TestFixture]
+    public class RangeTests
+    {
+        [Test]
+        public void RangeHasValidStartAndEndDelimitersTest()
+        {
+            Range r = new Range();
+            string input = "[2,6)";
+
+            bool validDelimiters = r.ValidInput(input);
+
+            Assert.That(validDelimiters, Is.True);
+        }
+
+        [Test]
+        public void InvalidStartAndEndDelimitersFailsTest()
+        {
+            Range r = new Range();
+            string input = "{2,6*";
+
+            bool validDelimiters = r.ValidInput(input);
+
+            Assert.That(validDelimiters, Is.False);
+        }
+
+        [Test]
+        public void ValidRangeContainsMinimumOfTwoIntegersTest()
+        {
+            Range r = new Range();
+            string input = "(a,6)";
+
+            bool validInput = r.ValidInput(input);
+
+            Assert.That(validInput, Is.True);
+        }
+
+        [Test]
+        public void CanCheckIfIntegerRangeContainsListTest()
+        {
+            Range r = new Range();
+            r.InputRange = "[2,6)";
+            List<int> integerList = new List<int>() { 2, 4 };
+
+            bool listInRange = r.ContainsList(integerList);
+
+            Assert.That(listInRange, Is.True);
+        }
+    }
+}
